a8fe311581487bb7409d21e760ffaa171ce3d86d
David Robert <contact@david-robert.fr>
2024-07-23 16:30:02 +0200

    form age poker

--- src/Controller/PokerController.php
+++ src/Controller/PokerController.php
@@ -13,13 +13,19 @@ class PokerController extends AbstractController
     public function poker() {
 
         $request = Request::createFromGlobals();
-        $age = $request->query->get('age');
 
-        if ($age >= 18) {
-            return $this->render('page/poker_welcome.html.twig');
+        if (!$request->query->has('age')) {
+            return $this->render('page/poker_form.html.twig');
         } else {
-            return $this->render('page/get_out.html.twig');
+            $age = $request->query->get('age');
+
+            if ($age >= 18) {
+                return $this->render('page/poker_welcome.html.twig');
+            } else {
+                return $this->render('page/get_out.html.twig');
+            }
         }
+
 
     }
 
