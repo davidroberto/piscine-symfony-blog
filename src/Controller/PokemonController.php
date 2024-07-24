e71c20556807b419d12a04a4671629b87ee003c8
David Robert <contact@david-robert.fr>
2024-07-24 09:52:03 +0200

    show pokemon by id

--- src/Controller/PokemonController.php
+++ src/Controller/PokemonController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -98,6 +99,93 @@ class PokemonController extends AbstractController
         ]);
 
         return new Response($html, 200);
+    }
+
+
+    #[Route('/pokemon-show', name: 'show_pokemon')]
+    public function showPokemon()
+    {
+        $request = Request::createFromGlobals();
+        $idPokemon = $request->query->get('id');
+
+        $pokemons = [
+            [
+                'id' => 1,
+                'title' => 'Carapuce',
+                'content' => 'Pokemon eau',
+                'isPublished' => true
+            ],
+            [
+                'id' => 2,
+                'title' => 'Salamèche',
+                'content' => 'Pokemon feu',
+                'isPublished' => true
+            ],
+            [
+                'id' => 3,
+                'title' => 'Bulbizarre',
+                'content' => 'Pokemon plante',
+                'isPublished' => true
+            ],
+            [
+                'id' => 4,
+                'title' => 'Pikachu',
+                'content' => 'Pokemon electrique',
+                'isPublished' => true
+            ],
+            [
+                'id' => 5,
+                'title' => 'Rattata',
+                'content' => 'Pokemon normal',
+                'isPublished' => false
+            ],
+            [
+                'id' => 6,
+                'title' => 'Roucool',
+                'content' => 'Pokemon vol',
+                'isPublished' => true
+            ],
+            [
+                'id' => 7,
+                'title' => 'Aspicot',
+                'content' => 'Pokemon insecte',
+                'isPublished' => false
+            ],
+            [
+                'id' => 8,
+                'title' => 'Nosferapti',
+                'content' => 'Pokemon poison',
+                'isPublished' => false
+            ],
+            [
+                'id' => 9,
+                'title' => 'Mewtwo',
+                'content' => 'Pokemon psy',
+                'isPublished' => true
+            ],
+            [
+                'id' => 10,
+                'title' => 'Ronflex',
+                'content' => 'Pokemon normal',
+                'isPublished' => false
+            ]
+
+        ];
+
+        $pokemonFound = null;
+
+        foreach ($pokemons as $pokemon) {
+            if($pokemon['id'] === (int)$idPokemon) {
+                $pokemonFound = $pokemon;
+            }
+        }
+
+        return $this->render('page/pokemon_show.html.twig', [
+            'pokemon' => $pokemonFound
+        ]);
+
+
+
     }
 
 
